c30fad8fbde52f49917b03d4d5cb98e8c5f45739
nikmanbro <nikmandaokar13@gmail.com>
2020-08-04 23:14:20 +0530

    Database Added

---
+++ E_CommerceShop/E_CommerceShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using E_CommerceShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceShop.DataAccess.SQL
+{
+   public class DataContext:DbContext
+    {
+     public DataContext() : base("DefaultConnection")
+        {
+        }
+        public DbSet<Products> Product { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+    }
+}
