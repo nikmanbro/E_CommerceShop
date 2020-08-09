5b0469b349d9be2bbd19d3bd282278a1c91b3226
nikmanbro <nikmandaokar13@gmail.com>
2020-08-09 12:47:10 +0530

    basket services part2

---
+++ E_CommerceShop/E_CommerceShop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using E_CommerceShop.Core.ViewModels;
+
+namespace E_CommerceShop.Core.Contracts
+{
+    public interface IBasketService
+    {
+        void AddToBasket(HttpContextBase httpContext, string productId);
+        List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+        void RemoveFromBasket(HttpContextBase httpContext, string itemId);
+    }
+}
